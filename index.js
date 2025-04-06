e60fc08a012e5b489877eea12f5d9b2cd7bda38e
Damoiskii <moimyazz@gmail.com>
2025-04-06 14:07:38 -0500

    Something else

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 document.getElementById("demo").style.display = "none";
 document.getElementById("next").style.display = "none";
 document.getElementById("previous").style.display = "none";
+document.getElementById("first").style.display = "none";
